87cc4372155307e4108afc779b58cbceb0825e74
Lam Nguyen <nguyentunglamvnm@gmail.com>
2024-08-15 11:06:29 +0700

    init

---
+++ Validator/Validator/Validator.h
@@ -0,0 +1,19 @@
+//
+//  MaccoValidator.h
+//  MaccoValidator
+//
+//  Created by Emmanuel Okwara on 18/09/2020.
+//  Copyright © 2020 Macco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaccoValidator.
+FOUNDATION_EXPORT double MaccoValidatorVersionNumber;
+
+//! Project version string for MaccoValidator.
+FOUNDATION_EXPORT const unsigned char MaccoValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaccoValidator/PublicHeader.h>
+
+
---
+++ Validator/Validator/Validator.swift
@@ -0,0 +1,21 @@
+//
+//  Validator.swift
+//  MaccoValidator
+//
+//  Created by Emmanuel Okwara on 18/09/2020.
+//  Copyright © 2020 Macco. All rights reserved.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func validEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    public static func sayHello() {
+        print("Hello pretty lady. How are you doing????")
+    }
+}
